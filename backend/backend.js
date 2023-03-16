56ec0046e137859b61c8e2fc647838f4f23b8d47
AlessioMurgia <alesave99@gmail.com>
2023-03-16 14:14:48 +0100

    fixed env var

--- backend/backend.js
+++ backend/backend.js
@@ -1,5 +1,4 @@
 const nodemailer = require('nodemailer');
-require('dotenv').config();
 
 function extractorHandler(formData) {
     const transporter = nodemailer.createTransport({
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config({path:'./backend/process.env'})
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
