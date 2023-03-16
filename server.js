089a0f2e7633e547fc1afc9cbdd91b992b7feca5
AlessioMurgia <alesave99@gmail.com>
2023-03-16 14:25:15 +0100

    fixed env var

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path:'./backend/process.env'})
+require('dotenv').config({path:'./process.env'})
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
